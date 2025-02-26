2f69cd531d977d72816356eae06ee86621bb0624
safinamoideen <122775542+moideensafina@users.noreply.github.com>
2025-02-26 22:05:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
+import Header from './components/header/header';
+import Home from './components/home/home';
+import About from './components/about/about';
+import Skills from './components/skills/skills';
+import Services from './components/services/services';
+import Qualification from './components/qualification/qualification';
+import Test from './components/test/test';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+import Scrollup from './components/scrollup/scrollup';
+const  App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <> 
+      <Header />
+      <main className='main'>
+        <Home/>
+        <About/>
+        <Skills/>
+        <Services/>
+        <Qualification/>
+        <Test/>
+        <Contact/>        
+      </main>
+       <Footer/>
+      <Scrollup/>
+       </>
   );
 }
 
