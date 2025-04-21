5139f138e5e44577405586357e63ac8a7d517fdf
safinamoideen <122775542+moideensafina@users.noreply.github.com>
2025-04-21 13:20:44 +0530

    c2

--- src/components/home/social.jsx
+++ src/components/home/social.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Social=()=>{
     return(
  <div className="home-social">
-    <a href="https://www.instagram.com/_._mr_moideen___/"  className="home-social-icon" target="_blank" >
+    <a href="https://www.instagram.com/_md_safie/"  className="home-social-icon" target="_blank" >
       <i class="uil uil-instagram"></i>
     </a>
     <a href="https://api.whatsapp.com/send?phone=9386452868&text=Hello%20Safina%0Ahow%20are%20you%0A" className="home-social-icon" target="_blank">
